22b3014a9f75b1bc6c6cbf729d6959d35db5aea8
Snehalkhairnar9 <L00163434@student.lyit.ie>
2021-11-05 11:01:48 +0000

    file added

---
+++ demo.java
@@ -0,0 +1,20 @@
+package demo_test;
+
+import org.junit.Test;
+
+/**
+ * @author sneha
+ *
+ */
+public class demo {
+
+	/**
+	 * @param args
+	 */
+	@Test
+	public void TestHelloworld()
+	{
+		System.out.println("hello world");
+	}
+
+}
